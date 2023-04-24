35316584a56958febf4c5df66205ca56c5375742
juanMa <juansalinas008@gmail.com>
2023-04-24 00:28:56 -0300

    probando para openlintspeed dejo como estaba

--- src/config/express.js
+++ src/config/express.js
@@ -1,5 +1,5 @@
-// import videosRouter from '#Routes/videos.routes.js';
-// import userRouter from '#Routes/user.routes.js';
+import videosRouter from '#Routes/videos.routes.js';
+import userRouter from '#Routes/user.routes.js';
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
@@ -10,19 +10,16 @@ console.log(path.join(process.cwd(), 'client/dist'));
 
 // Middlewares
 expressApp.use(cors());
-// expressApp.use(express.json());
-// expressApp.use(express.static(path.join(process.cwd(), 'client/dist')));
+expressApp.use(express.json());
+expressApp.use(express.static(path.join(process.cwd(), 'client/dist')));
 
 // Routes
-// expressApp.use('/user', userRouter);
+expressApp.use('/user', userRouter);
 
-// expressApp.use('/videos', videosRouter);
+expressApp.use('/videos', videosRouter);
 
 expressApp.get('*', (req, res) => {
-	// res.sendFile(path.join(process.cwd(), 'client/dist/index.html'));
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Probandooooo! From OpenLiteSpeed NodeJS\n');
+	res.sendFile(path.join(process.cwd(), 'client/dist/index.html'));
 });
 
 export default expressApp;
