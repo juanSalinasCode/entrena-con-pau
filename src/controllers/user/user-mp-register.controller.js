140d3cf9d818be2f581c27a48d28606d72d1718b
juanMa <juansalinas008@gmail.com>
2023-05-25 21:08:41 -0300

    pruebas notificaciones mp 2

--- src/controllers/user/user-mp-register.controller.js
+++ src/controllers/user/user-mp-register.controller.js
@@ -1,21 +1,21 @@
-import crypto from 'crypto';
+// import crypto from 'crypto';
 
 const userMpRegisterController = async (req, res) => {
-	const { type } = req.body;
-	// Verificar la firma del token
-	const accessToken =
-		'APP_USR-63212336526969-031321-33c1bfd1eb9c67cd33c70fd6ddb36d3a-206468902'; // 'TU_ACCESS_TOKEN'; // Reemplaza con tu propia clave secreta de Webhook
-	const receivedSignature = req.headers['x-payments-signature'];
-	const expectedSignature = crypto
-		.createHmac('sha256', accessToken)
-		.update(JSON.stringify(req.body))
-		.digest('hex');
+	// const { type } = req.body;
+	// // Verificar la firma del token
+	// const accessToken =
+	// 	'APP_USR-63212336526969-031321-33c1bfd1eb9c67cd33c70fd6ddb36d3a-206468902'; // 'TU_ACCESS_TOKEN'; // Reemplaza con tu propia clave secreta de Webhook
+	// const receivedSignature = req.headers['x-payments-signature'];
+	// const expectedSignature = crypto
+	// 	.createHmac('sha256', accessToken)
+	// 	.update(JSON.stringify(req.body))
+	// 	.digest('hex');
 
-	if (receivedSignature !== expectedSignature) {
-		return res.status(401).send('Firma del token no válida');
-	}
+	// if (receivedSignature !== expectedSignature) {
+	// 	return res.status(401).send('Firma del token no válida');
+	// }
 
-	if (type !== 'subscription') return res.status(409).send({ errors: [''] });
+	// if (type !== 'subscription') return res.status(409).send({ errors: [''] });
 
 	return res.status(201).send('Usuario registrado con éxito');
 };
