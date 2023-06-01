6ef5c266bce99e7aa3eee4d14cf3092b9aa06973
juanMa <juansalinas008@gmail.com>
2023-05-31 22:34:59 -0300

    probando webhook

--- src/config/express.js
+++ src/config/express.js
@@ -1,5 +1,6 @@
 import videosRouter from '#Routes/videos.routes.js';
 import userRouter from '#Routes/user.routes.js';
+import userMpRegisterController from '#Controllers/user/user-mp-register.controller.js';
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
@@ -21,5 +22,7 @@ expressApp.use('/videos', videosRouter);
 expressApp.get('*', (req, res) => {
 	res.sendFile(path.join(process.cwd(), 'client/dist/index.html'));
 });
+
+userRouter.post('/notificaciones', userMpRegisterController);
 
 export default expressApp;
